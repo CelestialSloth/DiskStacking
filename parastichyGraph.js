b2d071ca9be3fe970bf0b5a1c7f8e052cd585784
Elaine Demetrion <52363369+CelestialSloth@users.noreply.github.com>
2022-06-27 15:53:26 +0000

    Buttons in proper locations

---
+++ parastichyGraph.js
@@ -0,0 +1,7 @@
+/*Given the array of front data, this script outputs a canvas with that graph.
+@param frontData: the array of front data from a stackingCone
+@param parentDiv: the name of the div container to use as a parent class.
+*/
+function drawParastichyGraph(frontData, parentDiv) {
+  
+}
